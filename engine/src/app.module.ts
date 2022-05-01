68684b4b045553610ba14ba14185a50ab7e3e9b8
spRite75 <luke.montgomery@hotmail.com>
2022-05-01 10:40:07 +1000

    add local env configuration option

--- engine/src/app.module.ts
+++ engine/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { ParsingService } from './parsing/parsing.service';
 import { LoadingService } from './loading/loading.service';
 import { AnalyserService } from './analyser/analyser.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.ACTIVE_PROFILE || 'local'}.env`,
+      ignoreEnvFile: process.env.ACTIVE_PROFILE === 'live'
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ParsingService, LoadingService, AnalyserService],
 })
