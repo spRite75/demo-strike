ac303bab4b63bee016ff59e3a8f5e4619e13598a
spRite75 <luke.montgomery@hotmail.com>
2022-05-02 13:47:50 +1000

    use react 18, initialise firebase in app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkg3VmuoUYLlrUnvcm-lwFHSMfsn0T-Ns",
+  authDomain: "csgo-demo-analyser.firebaseapp.com",
+  projectId: "csgo-demo-analyser",
+  storageBucket: "csgo-demo-analyser.appspot.com",
+  messagingSenderId: "623562071555",
+  appId: "1:623562071555:web:ad4e0c4b5d50203a947836"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
